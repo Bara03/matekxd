4a0051498f3e036d850af84fac10a4d14097d69f
Bara03 <barabas.balazs03@gmail.com>
2023-05-21 16:46:21 +0200

    teljes valseg, bayes tetel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 import sys
-available_types=["Szórás/eloszlás","Kiválasztás"]
+available_types=["Szórás/eloszlás","Kiválasztás","Teljes valószínűség, bayes(egyforma ládák)"]
 class basicMath:
     def factorial(n:int)->int:
         if n<0: return -1
@@ -8,6 +8,9 @@ class basicMath:
         return n * basicMath.factorial(n-1)
     def NCR(n:int,r:int):
         return int(basicMath.factorial(n) /  ( basicMath.factorial(r) * basicMath.factorial( (n - r) ) ))
+
+    def get_fraction(input_number: float) -> str:
+        return str(Fraction(str(input_number)).limit_denominator(1000))
 
 class dis:
     def get_E(self,input_dict: dict) -> float:
@@ -29,9 +32,6 @@ class dis:
         E_ = self.get_E(input_dict) ** 2
         E_pow = self.get_Epow(input_dict)
         return ((E_pow - E_) ** 0.5)
-
-    def get_fraction(self,input_number: float) -> str:
-        return str(Fraction(str(input_number)).limit_denominator(1000))
 
     def process_fraction(self,input_dict: dict) -> dict:
         return_dict = {}
@@ -61,7 +61,7 @@ class dis:
         print(
             f"\033[33mA megadott feladathoz az eloszlás: E(x)= \033[92m{results['E_simple']}\033[33m\nA szórás pedig: D(x)= \033[92m{results['D_']}\n\033[0m")
         print(
-            f"\033[33mTört alakok az alábbiak:\nE(x)= \033[92m{self.get_fraction(results['E_simple'])}\033[33m\nE2(x)= \033[92m{self.get_fraction(results['E_powered'])}\033[33m\nD(x)= \033[92m{self.get_fraction(results['D_'])}\033[0m")
+            f"\033[33mTört alakok az alábbiak:\nE(x)= \033[92m{basicMath.get_fraction(results['E_simple'])}\033[33m\nE2(x)= \033[92m{basicMath.get_fraction(results['E_powered'])}\033[33m\nD(x)= \033[92m{basicMath.get_fraction(results['D_'])}\033[0m")
 
 class select:
     def probability(self,basket:dict,selected:int,positive:int,who:str,type:int)->int:
@@ -100,6 +100,43 @@ class select:
         who = input(f"Kit választunk ki? (nevet ird be):")
         print(f"\033[33mA megadott feladat valószínűsége kerekítve: \033[92m{round(self.probability(input_basket, selected_count, positive, who, type),3)}\033[0m")
 
+class full_prob:
+    def get_full_prob(raw_list, selected):
+        selected -= 1
+        all_prob = 1 / len(raw_list)
+        return sum(v[selected] / sum(v) * all_prob for v in raw_list if v[selected] > 0)
+    def get_bayes(raw_list,selected,wich):
+        all_P = full_prob.get_full_prob(raw_list,selected)
+        selected -=1
+        wich = wich-1
+        all_prob = 1 / len(raw_list)
+        return (( raw_list[wich][selected] / sum(raw_list[wich]) ) * all_prob) / all_P
+    def main(self):
+        input_list = []
+        print("Add meg a hány értéket tartalmaz: ")
+        nm = 1
+        print(f"A{nm}: ", end="")
+        while (ipt := input()) != "":
+            nm += 1
+            # print(f"\tx{nm2}: ",end="")
+            hold_list = []
+            for i in range(0, int(ipt)):
+                hold_list.append(int(input(f"\tx{i + 1}: ")))
+            # while (ipt2 := input()) != "":
+            #     hold_list.append(int(ipt2))
+            #     print(f"\tx{nm2}: ", end="")
+            input_list.append(hold_list)
+            print(f"A{nm}: ", end="")
+        for v, k in input_list:
+            print(f"[1]: {v}, [2]: {k}")
+        selected_num = int(input("Melyik adat valószínűségére van szükséged? (szám): "))
+        for i in range(len(input_list)):
+            print(f"[{i + 1}] - A{i + 1}")
+        wich = int(input("Melyik A-ban vizsgáljuk a bayes tételt? (szám): "))
+        val = full_prob.get_full_prob(input_list, selected_num)
+        bay = full_prob.get_bayes(input_list, selected_num, wich)
+        print(f"Teljes valség: {val} tört: {basicMath.get_fraction(str(val))}")
+        print(f"Bayes: {bay} tört: {basicMath.get_fraction(str(bay))}")
 def main():
     for i,v in enumerate(available_types, start=1):
         print(f"[{i}] : {v}")
@@ -109,6 +146,8 @@ def main():
             dis().main()
         elif(chosen_type == "2"):
             select().main()
+        elif(chosen_type == "3"):
+            full_prob().main()
         else:
             raise ValueError("Nincs ilyen típus!")
     except ValueError as e:
